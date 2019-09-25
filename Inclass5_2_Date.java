07c2d029a55206f588d0ebe39dbbdc70728aae3a
chasedarlington <54547615+chasedarlington@users.noreply.github.com>
2019-09-25 16:00:08 -0700

    Add files via upload

---
+++ Inclass5_2_Date.java
@@ -0,0 +1,50 @@
+public class Inclass5_2_Date{
+	private int day, year; //initialize
+	private String month; //initialize
+
+	public Inclass5_2_Date() { //constructor
+		month=" ";
+		day=0;
+		year=0;
+	}
+
+	public Inclass5_2_Date(String m, int d, int y) { //constructor overloading; accepts initial inputs
+		month=m;
+		day=d;
+		year=y;
+	}
+
+	public void set_date(String m, int d, int y) { //mutator method
+		month=m;day=d;year=y;
+	}
+
+	public void set_month(String m) { //mutator method
+		month=m;
+	}
+
+	public void set_day(int d) { //mutator method
+		day=d;
+	}
+
+	public void set_year(int y) { //mutator method
+		year=y;
+	}
+
+	public String get_month() { //accessor method
+		return month;
+	}
+
+	public int get_day() { //accessor method
+		return day;
+	}
+
+	public int get_year() { //accessor method
+		return year;
+	}
+
+	public String toString() { //output to string (essentially a get_date method)
+			return (month+"/"+day+"/"+year);
+		}
+
+
+}
